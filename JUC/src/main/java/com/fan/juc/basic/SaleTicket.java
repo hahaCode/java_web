4f747f09a9c867dd5287f295e3401536ebeacd4c
hahaCode <651409869@qq.com>
2022-11-07 22:40:01 +0800

    juc basic init commit

---
+++ JUC/src/main/java/com/fan/juc/basic/SaleTicket.java
@@ -0,0 +1,26 @@
+package com.fan.juc.basic;
+
+
+class Ticket{
+    private int number = 30;
+
+    public synchronized void sale() {
+        if (number > 0) {
+            System.out.println(Thread.currentThread().getName() + "：卖出：" + number-- + "; 剩余： "+ number);
+        }
+    }
+}
+
+public class SaleTicket {
+    public static void main(String[] args) {
+        Ticket ticket = new Ticket();
+
+        for (int j = 0; j < 3; j++) {
+            new Thread(() -> {
+                for (int i = 0; i < 40; i++) {
+                    ticket.sale();
+                }
+            }, "Thread_"+(j+1)).start();
+        }
+    }
+}
