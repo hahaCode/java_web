0f3a8b83c5c9200d8ff3daab5f65e4f6e4069429
hahaCode <651409869@qq.com>
2023-03-30 21:55:34 +0800

    backtrack problem

---
+++ algo/src/main/java/com/fan/algo/Subsets.java
@@ -0,0 +1,31 @@
+package com.fan.algo;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Subsets {
+    public static void main(String[] args) {
+
+    }
+
+    List<List<Integer>> subset = new LinkedList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+
+    public List<List<Integer>> subsets(int[] nums) {
+
+        backtrack(nums, 0);
+        return subset;
+    }
+
+    public void backtrack(int[] nums, int level) {
+
+        subset.add(new ArrayList<>(track));
+
+        for (int i = level; i < nums.length; i++) {
+            track.add(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+}
