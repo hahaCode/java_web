982ddf5fd67e3d5c69132e61b5c95a56fa7d3451
hahaCode <651409869@qq.com>
2023-04-07 11:25:14 +0800

    queens

---
+++ algo/src/main/java/com/fan/algo/NQueens.java
@@ -0,0 +1,79 @@
+package com.fan.algo;
+
+import java.io.FilterOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NQueens {
+    public static void main(String[] args) {
+        NQueens nQueens = new NQueens();
+        nQueens.solveNQueens(4);
+
+        //System.out.println(nQueens.res.get(0).get(0) + " " + nQueens.res.get(0).get(1) + " " + nQueens.res.get(0).get(2) + "" + nQueens.res.get(0).get(3));
+        //System.out.println(nQueens.res.get(1).get(0) + " " + nQueens.res.get(1).get(1) + " " + nQueens.res.get(1).get(2) + "" + nQueens.res.get(1).get(3));
+    }
+
+    List<List<String>> res = new ArrayList<>();
+    char[][] table;
+
+    public List<List<String>> solveNQueens(int n) {
+        table = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                table[i][j] = '.';
+            }
+        }
+
+        backtrack(n, 0);
+        return res;
+    }
+
+    public void backtrack(int n, int cur) {
+        if (cur == n) {
+            List<String> s = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                String temp = "";
+                for (int j = 0; j < n; j++) {
+                    temp += table[i][j];
+                }
+                s.add(temp);
+            }
+
+            //System.out.println("===>" + s);
+            res.add(s);
+            return;
+        }
+
+
+        for (int j = 0; j < n; j++) {
+            if (table[cur][j] == '.' && validate(cur, j, n)) {
+                table[cur][j] = 'Q';
+                backtrack(n, cur + 1);
+                table[cur][j] = '.';
+            }
+        }
+
+    }
+
+    public boolean validate(int x, int y, int n) {
+
+        //上
+        for (int i = 0; i < x; i++) {
+            if (table[i][y] == 'Q') return false;
+        }
+
+        //左上
+        for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+            if (table[i][j] == 'Q') return false;
+        }
+
+        //右上
+        for (int i = x, j = y; i >= 0 && j < n; i--, j++) {
+            if (table[i][j] == 'Q') return false;
+        }
+
+        return true;
+    }
+}
